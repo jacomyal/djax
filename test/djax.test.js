b00b917410d10b76e4add5c9a05af4622196fde2
Alexis Jacomy <alexis.jacomy@gmail.com>
2014-11-03 18:14:40 +0100

    Updated tests to take into account last 2 commits
     - Using "success" instead of "ok" as status
     - Checking that the xhr argument given to callbacks is the same object as the one returned by the ajax function instead of checking that it is an instasnce of XHR
     - Added a test to check that the .done() method works even after the call has been ended (ie when the "done" flag is true)

--- test/djax.test.js
+++ test/djax.test.js
@@ -3,12 +3,12 @@ var assert = require('assert'),
 
 describe('Polymorphism', function() {
   it('should work as ajax({ url, success, error })', function(done) {
-    ajax({
+    var res = ajax({
       url: '/data/1',
       success: function(data, textStatus, xhr) {
         assert.deepEqual(data.result, { data: 'abcde', id: '1' });
-        assert(textStatus === 'ok');
-        assert(xhr instanceof ajax.xhr);
+        assert(textStatus === 'success');
+        assert(xhr === res);
         assert(arguments.length === 3);
         done();
       },
@@ -19,12 +19,12 @@ describe('Polymorphism', function() {
   });
 
   it('should work as ajax(url).then(success, error)', function(done) {
-    ajax('/data/1')
+    var res = ajax('/data/1')
       .then(
         function(data, textStatus, xhr) {
           assert.deepEqual(data.result, { data: 'abcde', id: '1' });
-          assert(textStatus === 'ok');
-          assert(xhr instanceof ajax.xhr);
+          assert(textStatus === 'success');
+          assert(xhr === res);
           assert(arguments.length === 3);
           done();
         },
@@ -34,12 +34,12 @@ describe('Polymorphism', function() {
   });
 
   it('should work as ajax(url).done(success).fail(error)', function(done) {
-    ajax('/data/1')
+    var res = ajax('/data/1')
       .done(
         function(data, textStatus, xhr) {
           assert.deepEqual(data.result, { data: 'abcde', id: '1' });
-          assert(textStatus === 'ok');
-          assert(xhr instanceof ajax.xhr);
+          assert(textStatus === 'success');
+          assert(xhr === res);
           assert(arguments.length === 3);
           done();
         })
@@ -50,7 +50,7 @@ describe('Polymorphism', function() {
   });
 
   it('should work as ajax(url).fail(error).done(success)', function(done) {
-    ajax('/data/1')
+    var res = ajax('/data/1')
       .fail(
         function() {
           throw new Error('Unexpected error.');
@@ -58,22 +58,40 @@ describe('Polymorphism', function() {
       .done(
         function(data, textStatus, xhr) {
           assert.deepEqual(data.result, { data: 'abcde', id: '1' });
-          assert(textStatus === 'ok');
-          assert(xhr instanceof ajax.xhr);
+          assert(textStatus === 'success');
+          assert(xhr === res);
           assert(arguments.length === 3);
           done();
+        });
+  });
+
+  it('should work with res.done called after success', function(done) {
+    var res = ajax('/data/1')
+      .fail(
+        function() {
+          throw new Error('Unexpected error.');
+        })
+      .done(
+        function() {
+          res.done(function(data, textStatus, xhr) {
+            assert.deepEqual(data.result, { data: 'abcde', id: '1' });
+            assert(textStatus === 'success');
+            assert(xhr === res);
+            assert(arguments.length === 3);
+            done();
+          });
         });
   });
 });
 
 describe('HTTP verbs', function() {
   it('should work with GET calls', function(done) {
-    ajax({
+    var res = ajax({
       url: '/data/1',
       success: function(data, textStatus, xhr) {
         assert.deepEqual(data.result, { data: 'abcde', id: '1' });
-        assert(textStatus === 'ok');
-        assert(xhr instanceof ajax.xhr);
+        assert(textStatus === 'success');
+        assert(xhr === res);
         assert(arguments.length === 3);
         done();
       },
@@ -84,7 +102,7 @@ describe('HTTP verbs', function() {
   });
 
   it('should work with POST calls', function(done) {
-    ajax({
+    var res = ajax({
       url: '/data/1',
       type: 'POST',
       contentType: 'application/json',
@@ -93,8 +111,8 @@ describe('HTTP verbs', function() {
       },
       success: function(data, textStatus, xhr) {
         assert.deepEqual(data.result, { data: 'DEFGH', id: '1' });
-        assert(textStatus === 'ok');
-        assert(xhr instanceof ajax.xhr);
+        assert(textStatus === 'success');
+        assert(xhr === res);
         assert(arguments.length === 3);
         done();
       },
@@ -105,7 +123,7 @@ describe('HTTP verbs', function() {
   });
 
   it('should work with PUT calls', function(done) {
-    ajax({
+    var res = ajax({
       url: '/data/',
       type: 'PUT',
       contentType: 'application/json',
@@ -114,8 +132,8 @@ describe('HTTP verbs', function() {
       },
       success: function(data, textStatus, xhr) {
         assert.deepEqual(data.result, { data: 'First entry', id: '3' });
-        assert(textStatus === 'ok');
-        assert(xhr instanceof ajax.xhr);
+        assert(textStatus === 'success');
+        assert(xhr === res);
         assert(arguments.length === 3);
         done();
       },
@@ -126,13 +144,13 @@ describe('HTTP verbs', function() {
   });
 
   it('should work with DELETE calls', function(done) {
-    ajax({
+    var res = ajax({
       url: '/data/2',
       type: 'DELETE',
       success: function(data, textStatus, xhr) {
         assert.deepEqual(data, { ok: true });
-        assert(textStatus === 'ok');
-        assert(xhr instanceof ajax.xhr);
+        assert(textStatus === 'success');
+        assert(xhr === res);
         assert(arguments.length === 3);
         done();
       },
@@ -143,14 +161,14 @@ describe('HTTP verbs', function() {
   });
 
   it('should trigger the error callback when status is 404', function(done) {
-    ajax({
+    var res = ajax({
       url: '/data/inexisting_id',
       type: 'POST',
       success: function(data, textStatus, xhr) {
         throw new Error('Unexpected success.');
       },
       error: function(xhr, textStatus, errorThrown) {
-        assert(xhr instanceof ajax.xhr);
+        assert(xhr === res);
         assert(textStatus === 'error');
         assert(errorThrown === 'Bad request');
         assert(arguments.length === 3);
